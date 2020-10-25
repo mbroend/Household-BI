b6e879579adceedfe7e1a827744733f04940b812
Catharina Holland <catharinaholland@Catharinas-MacBook-Pro.local>
2020-10-25 21:56:34 +0100

    General updates and functionality added

--- Dash/app.py
+++ Dash/app.py
@@ -6,91 +6,192 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+
+from datetime import date
+
 import plotly.express as px
+
+
 import pandas as pd
 import numpy as np
+from scripts.data_wrangling import load_data
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+#external_stylesheets = ['css/style.css']
+#c    'background': '#111111',
+#    'text': '#7FDBFF'
+#}
+app = dash.Dash(__name__,title='Household Analytics')#, external_stylesheets=external_stylesheets)
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# assume you have a "long-form" data frame
-df = pd.read_csv('../data/export.csv',delimiter=';', header=None, names=['Date','Date1','Post','Value','Acc_Value'])
-df.drop(columns=['Date1'],inplace=True)
-df.Value = df.Value.str.replace('.','').str.replace(',','.').astype('float64')
-df.Acc_Value = df.Acc_Value.str.replace('.','').str.replace(',','.').astype('float64')
-df['DateTime'] = pd.to_datetime(df.Date, dayfirst=True)
-df.sort_values('DateTime',inplace= True)
-df['Category'] = np.select([df['Post'].str.contains(r'(?i)(netto)') |
-                            df['Post'].str.contains(r'(?i)(rema)') |
-                            df['Post'].str.contains(r'(?i)(føtex)') |
-                            df['Post'].str.contains(r'(?i)(meny)') |
-                            df['Post'].str.contains(r'(?i)(lidl)') |
-                            df['Post'].str.contains(r'(?i)(superb)')
-                            ,
-                            df['Post'].str.contains(r'(?i)(mobility)') |
-                            df['Post'].str.contains(r'(?i)(dsb)') |
-                            df['Post'].str.contains(r'(?i)(kombardo)') |
-                            df['Post'].str.contains(r'(?i)(billeje)') |
-                            df['Post'].str.contains(r'(?i)(easypark)') |
-                            df['Post'].str.contains(r'(?i)(parkering)') |
-                            df['Post'].str.contains(r'(?i)(lej1lig)') |
-                            df['Post'].str.contains(r'(?i)(diesel)') |
-                            df['Post'].str.contains(r'(?i)(benzin)') |
-                            df['Post'].str.contains(r'(?i)(one2move)') |
-                            df['Post'].str.contains(r'(?i)(molslinien)') |
-                            df['Post'].str.contains(r'(?i)(færge)') |
-                            df['Post'].str.contains(r'(?i)(storebælt)') |
-                            df['Post'].str.contains(r'(?i)(norwegian)') |
-                            df['Post'].str.contains(r'(?i)(bil^[a-zA-Z])')
-                            ,
-                            df['Post'].str.contains(r'(?i)(ikea)') |
-                            df['Post'].str.contains(r'(?i)(silvan)') |
-                            df['Post'].str.contains(r'(?i)(stark)')
-                            ,
-                            df['Post'].str.contains(r'(?i)(elgiganten)')
-                            ,
-                            df['Post'].str.contains(r'(?i)(gave)')
-                            ,
-                            df['Post'].str.contains(r'(?i)(wolt)') |
-                            df['Post'].str.contains(r'(?i)(pizza)')
-                           ],
-                           ['Dagligvarer', 'Transport', 'Bolig', 'Elektronik', 'Gave', 'Takeout'],
-                           default = 'Andet'
+df,df2,dg = load_data()
+def bar_style(fig):
+    fig.update_xaxes(showgrid=False,title='')
+    fig.update_yaxes(title='')
+    fig.update_layout(paper_bgcolor='rgba(0,0,0,0)')
+    fig.update_layout(plot_bgcolor='rgba(0,0,0,0)')
+    return
 
-                          )
+fig2 = px.line(df, x="DateTime",
+                y="Acc_Value", hover_data=['Post','Value'],
+                title='Akkumuleret værdi over tid')
+fig3 = px.bar(dg, x="DateTime", y="Value", color='Category',title='Udgifter fordelt på kategorier')
+fig3.update_xaxes(showgrid=False)
+fig3.update_yaxes(showgrid=False)
+fig3.update_layout(paper_bgcolor='rgba(0,0,0,0)')
+bar_style(fig3)
 
-dg = df.query('Value < 0').groupby([pd.Grouper(key='DateTime', freq='1M'),'Category']).sum() # groupby each 1 month
-dg.reset_index(inplace=True)
-dg.DateTime= dg.DateTime.dt.strftime('%Y-%B')
-dg.Value = dg.Value*-1
-# see https://plotly.com/python/px-arguments/ for more options
-df2 = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+cardstyle = {'borderRadius': '5px',
+'backgroundColor':'#f9f9f9',
+'margin':'10px',
+'padding': '15px',
+'position': 'relative',
+'boxShadow': '2px 2px 2px lightgrey'}
 
-fig = px.bar(df2, x="Fruit", y="Amount", color="City", barmode="group")
+unique_months = dg['DateTime'].drop_duplicates().reset_index(drop=True)
+max_slider_steps = unique_months.shape[0]-1
 
-fig2 = px.line(df, x="DateTime", y="Acc_Value", hover_data=['Post','Value'], title='Life expectancy in Canada')
-fig3 = px.bar(dg, x="DateTime", y="Value", color='Category',title='Udgifter')
-app.layout = html.Div(children=[
-    html.H1(children='Household BI'),
+budget_layout = html.Div(#className='container',
+children=[
+html.Div(children = [
+    html.Div(
+        className='row flex',
+        children=[
+            html.Div(
+                className = 'four columns',
+                children=[
+                    html.Div(className = 'mini_container', id='filters', children =[
+                    html.P('Vælg dato interval'),
+                    html.Div(className = 'slider_container', children = [
+                    dcc.RangeSlider(
+    id='month-slider',
+    min = 0,
+    max = max_slider_steps,
+    step = None,
+    marks = {key: {'label':value[0:max_slider_steps],'style':{'transform': 'rotate(-45deg)','font-size':'8px','left':str(100/max_slider_steps*(key)-100/max_slider_steps    )+'%'}} for (key, value) in dict(unique_months).items()},
+    value = [max_slider_steps-6,max_slider_steps]
+    #className = 'rotate'
+    )]),
+    html.Div(id='slider-output-container'),
+                        dcc.DatePickerRange(id='date-picker-range',
+                                min_date_allowed=date(1995, 8, 5),
+                                max_date_allowed=date(2030, 12, 31),
+                                start_date = date(2020, 1, 1),
+                                end_date=date(2020, 12, 31),
+                                display_format='DD/MM/YYYY',
+                          #'background-color': '#f9f9f9'}
+                            #
+                            )
+                                ])
+                    ]),
+            html.Div(
+                className='eight columns',
+                children = [
+                    html.Div(className = 'row container-display', children = [
+                        html.Div(className = 'mini_container', id = 'card1',
+                            children = [
+                                html.H6(id='lastest-expense',children=['FLASHY CARD']),
+                                html.P('Total forbrug seneste måned')
+                            ]),
+                        html.Div(className = 'mini_container', id ='card2',
+                                children = [
+                                    html.H6(id='percentile-groc',children=['0']),
+                                    html.P('Dagligvarerandel seneste måned')
+                                ]),
+                    ]),
+                    html.Div(className = 'mini_container',
+                        children = [
+                        dcc.Graph(id='expenditures',figure=fig3)
+                        ])
+                ])
+        ]),
+    html.Div(
+        className='row flex',
+        children=[
+        html.Div(
+            className='twelve columns',
+            children = [
+            html.Div(className = 'mini_container', children =[
+            ''' Vælg dato interval ''',
 
-    html.Div(children='''
-        Many moneyz
-    '''),
+                dcc.DatePickerRange(id='date-picker-range',
+                        min_date_allowed=date(1995, 8, 5),
+                        max_date_allowed=date(2030, 12, 31),
+                        start_date = date(2020, 1, 1),
+                        end_date=date(2020, 12, 31),
+                        display_format='DD/MM/YYYY',
 
-    dcc.Graph(
-        id='acc-value',
-        figure=fig2
-    ),
-    dcc.Graph(
-        id='expenditures',
-        figure=fig3
-    )
+                        ),
+
+                        dcc.Graph(id='acc-value',figure=fig2,
+                        style = {'background-color': '#f9f9f9'})
+                    ])
+
+    ])
+])
+])
 ])
 
+
+app.layout = html.Div(children=[
+    html.H1(children='Household BI'),
+    html.Div([
+        dcc.Location(id="url"),
+        html.Div([
+            dcc.Link('Budget', href="/Budget"),
+            dcc.Link('Aktier', href="/Aktier"),
+            dcc.Link('Crowdlending', href="/Crowdlending")
+        ]),
+        html.Div(id='content-field-in-app-layout')
+    ])
+])
+
+app.config['suppress_callback_exceptions']=True
+@app.callback(
+    Output(component_id='acc-value', component_property='figure'),
+    [Input(component_id='date-picker-range', component_property='start_date'),
+    Input(component_id='date-picker-range', component_property='end_date')]
+)
+def update_output_div(start_date,end_date):
+    return px.line( df[(df['DateTime']>=start_date)&(df['DateTime']<=end_date)],
+                    x = 'DateTime',
+                    y = 'Acc_Value',
+                    hover_data=['Post','Value'],
+                    title='Akkumuleret værdi over tid'
+                )
+
+@app.callback(
+    [Output('expenditures', component_property='figure'),
+     Output('lastest-expense', component_property='children'),
+     Output('percentile-groc', component_property='children')
+    ],
+    [Input('month-slider', 'value')])
+
+def update_output(value):
+    selected_months = unique_months.loc[value[0]:value[1]]
+    fig = px.bar(dg[dg['DateTime'].isin(selected_months)],
+                x="DateTime", y="Value",
+                color='Category',title='Udgifter fordelt på kategorier')
+    bar_style(fig)
+    lastest_month = unique_months.loc[value[1]]
+    latest_expense = dg[dg['DateTime'] == lastest_month]['Value'].sum()
+    percentile_groc = 100*dg[(dg['DateTime'] == lastest_month) & (dg['Category'] == 'Dagligvarer')]['Value']/latest_expense
+    percentile_groc = round(percentile_groc,1)
+    return fig, latest_expense, percentile_groc
+
+@app.callback(Output('content-field-in-app-layout', 'children'),
+[Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == "/Budget":
+   	    return budget_layout
+    elif pathname == "/":
+        return budget_layout
+    elif pathname == "/Aktier":
+   	    return Marketing_layout
+    elif pathname == "/Crowdlending":
+   	    return Finance_layout
+    else:
+   	    return Error_display_layout
+
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True,port=8080,host='0.0.0.0')
---
+++ Dash/scripts/data_wrangling.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+def load_data():
+    # assume you have a "long-form" data frame
+    df = pd.read_csv('../data/export.csv',delimiter=';', header=None, names=['Date','Date1','Post','Value','Acc_Value'])
+    df.drop(columns=['Date1'],inplace=True)
+    df.Value = df.Value.str.replace('.','').str.replace(',','.').astype('float64')
+    df.Acc_Value = df.Acc_Value.str.replace('.','').str.replace(',','.').astype('float64')
+    df['DateTime'] = pd.to_datetime(df.Date, dayfirst=True)
+    df.sort_values('DateTime',inplace= True)
+    df['Category'] = np.select([df['Post'].str.contains(r'(?i)(netto)') |
+                                df['Post'].str.contains(r'(?i)(rema)') |
+                                df['Post'].str.contains(r'(?i)(føtex)') |
+                                df['Post'].str.contains(r'(?i)(meny)') |
+                                df['Post'].str.contains(r'(?i)(lidl)') |
+                                df['Post'].str.contains(r'(?i)(superb)')
+                                ,
+                                df['Post'].str.contains(r'(?i)(mobility)') |
+                                df['Post'].str.contains(r'(?i)(dsb)') |
+                                df['Post'].str.contains(r'(?i)(kombardo)') |
+                                df['Post'].str.contains(r'(?i)(billeje)') |
+                                df['Post'].str.contains(r'(?i)(easypark)') |
+                                df['Post'].str.contains(r'(?i)(parkering)') |
+                                df['Post'].str.contains(r'(?i)(lej1lig)') |
+                                df['Post'].str.contains(r'(?i)(diesel)') |
+                                df['Post'].str.contains(r'(?i)(benzin)') |
+                                df['Post'].str.contains(r'(?i)(one2move)') |
+                                df['Post'].str.contains(r'(?i)(molslinien)') |
+                                df['Post'].str.contains(r'(?i)(færge)') |
+                                df['Post'].str.contains(r'(?i)(storebælt)') |
+                                df['Post'].str.contains(r'(?i)(norwegian)') |
+                                df['Post'].str.contains(r'(?i)(bil^[a-zA-Z])')
+                                ,
+                                df['Post'].str.contains(r'(?i)(ikea)') |
+                                df['Post'].str.contains(r'(?i)(silvan)') |
+                                df['Post'].str.contains(r'(?i)(stark)')
+                                ,
+                                df['Post'].str.contains(r'(?i)(elgiganten)')
+                                ,
+                                df['Post'].str.contains(r'(?i)(gave)')
+                                ,
+                                df['Post'].str.contains(r'(?i)(wolt)') |
+                                df['Post'].str.contains(r'(?i)(pizza)')
+                                ],
+                                ['Dagligvarer', 'Transport', 'Bolig', 'Elektronik', 'Gave', 'Takeout'],
+                                default = 'Andet'
+
+                                )
+
+    dg = df.query('Value < 0').groupby([pd.Grouper(key='DateTime', freq='1M'),'Category']).sum() # groupby each 1 month
+    dg.reset_index(inplace=True)
+    dg.DateTime= dg.DateTime.dt.strftime('%Y-%B')
+    dg.Value = dg.Value*-1
+    # see https://plotly.com/python/px-arguments/ for more options
+    df2 = pd.DataFrame({
+        "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+        "Amount": [4, 1, 2, 2, 4, 5],
+        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+    })
+    return df, df2, dg
